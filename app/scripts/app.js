bb61fa4152281c6be3f8c8f04e1cd6927f370998
Joe Bumbulis <joebumbulis@gmail.com>
2017-04-10 14:35:26 -0500

    starting react components

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,5 +4,35 @@ import AppRoot from './views/app_root.js'
 
 export default function app() {
 
-  render(<AppRoot />, document.querySelector('#app'));
+const BlogBody = React.createClass({
+    render: function(){
+      return(<article>
+        <h2>{this.props.title}</h2>
+        <h3>{this.props.time}</h3>
+        <p>{this.props.body}</p>
+        </article>)
+    }
+})
+
+const Content = React.className({
+  render: function(){
+    <BlogBody title="" time="" body=""/>
+    <BlogBody title="" time="" body=""/>
+    <BlogBody title="" time="" body=""/>
+    <BlogBody title="" time="" body=""/>
+    <BlogBody title="" time="" body=""/>
+  }
+})
+
+
+
+const App = React.createClass({
+  render: function() {
+    return (<main>
+      < Content/>
+    </main>)
+  }
+})
+
+  render(<App />, document.querySelector('#app'));
 }
