a00f39ea8b10046dd01ffcf91470e98a032a65a5
Joe Bumbulis <joebumbulis@gmail.com>
2017-04-10 15:43:36 -0500

    created blog input

--- app/scripts/views/app_root.js
+++ app/scripts/views/app_root.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import BlogComponent from './blog_component'
+import BlogInput from './blog_input'
 
 export default React.createClass({
   render: function () {
     return (
       (<main>
+        <BlogInput/>
         <BlogComponent/>
       </main>)
     );
---
+++ app/scripts/views/blog_input.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default React.createClass({
+  // getInitialState: function(){
+  //   return {
+  //     hidden: false
+  //   };
+  // },
+  render: function(){
+    return <aside>
+    <input type="text" />
+    <textarea rows="10" cols="50" />
+    </aside>
+  }
+})
